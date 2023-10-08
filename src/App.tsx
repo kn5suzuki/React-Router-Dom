39c61c15447d78ad271a1f09ebc30bc69ab95580
kn5suzuki <suzukikengo@g.ecc.u-tokyo.ac.jp>
2023-10-08 22:37:05 +0900

    simplest app

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React Router v6</h1>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
